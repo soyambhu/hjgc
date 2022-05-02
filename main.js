0ef1b50a3ff92c88bd6cd4241559d4a4dfa7282a
soyambhu <85630358+soyambhu@users.noreply.github.com>
2022-05-02 15:50:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ function setup()
 function draw()
 {
     image(img , 0, 0, 640, 420);
-    fill("#FF0000");
+    fill("#ff0000");
     text("Dog",45 , 75);
     noFill();
-    stroke("#FF0000");
+    stroke("#ff0000");
     rect(30, 60, 450, 350 );
 
 }
